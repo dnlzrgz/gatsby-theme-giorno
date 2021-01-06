a21db7fb3a686d55b5eca1a1305490c412a5919b
Daniel <danielkvist@protonmail.com>
2021-01-06 18:18:05 +0100

    chore(gatsby): add gatsby-source-plugin

--- gatsby-theme-giorno/gatsby-config.js
+++ gatsby-theme-giorno/gatsby-config.js
@@ -1,3 +1,22 @@
+const path = require('path');
+require('dotenv').config({
+	path: path.resolve('../.env'),
+});
+
 module.exports = {
-	plugins: ['gatsby-plugin-postcss'],
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-postcss',
+		{
+			resolve: 'gatsby-source-sanity',
+			options: {
+				// TODO: make they be received by theme options
+				projectId: process.env.SANITY_PROJECT_ID,
+				dataset: process.env.SANITY_DATASET_NAME,
+				token: process.env.SANITY_TOKEN,
+				watchMode: true,
+				overlayDrafts: true,
+			},
+		},
+	],
 };
