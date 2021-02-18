9c77e4588ecab8ef274ac34c6706707532e659b8
danielkvist <daniel@dkvist.com>
2021-02-18 23:02:40 +0100

    feat(gatsby): add basic footer grid styles

--- gatsby-theme-giorno/src/components/FooterCopyright.js
+++ gatsby-theme-giorno/src/components/FooterCopyright.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FooterCopyright = ({ copy }) => {
 	return (
-		<section>
+		<section className="col-span-1 sm:col-span-2 md:col-span-3">
 			<main className="text-center px-12">
 				<p>Copyright &copy; - {copy}</p>
 			</main>
