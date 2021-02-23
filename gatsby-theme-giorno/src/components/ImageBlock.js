4c9ae4a0e8d790fa2f14dfe33e8aff64d0c42d67
danielkvist <daniel@dkvist.com>
2021-02-23 16:11:34 +0100

    fix(gatsby): fix issue with overflow images

--- gatsby-theme-giorno/src/components/ImageBlock.js
+++ gatsby-theme-giorno/src/components/ImageBlock.js
@@ -6,9 +6,9 @@ import clientConfig from '../utils/clientConfig';
 
 const ImageBlock = ({ node }) => {
 	if (!node || !node.asset || !node.asset._ref) return null;
-
 	const fluid = getFluidGatsbyImage(node.asset._ref, {}, clientConfig.sanity);
-	return <Img fluid={fluid} />;
+
+	return <Img fluid={fluid} fadeIn className="object-contain" />;
 };
 
 export default ImageBlock;
