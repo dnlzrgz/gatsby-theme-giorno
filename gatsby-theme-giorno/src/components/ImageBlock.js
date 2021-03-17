75fb0e47f278e77a6b75e7b8dc8b53a21c4f9b07
danielkvist <daniel@dkvist.com>
2021-03-17 10:51:58 +0100

    chore(gatsby): update to gatsby v3.0.0

--- gatsby-theme-giorno/src/components/ImageBlock.js
+++ gatsby-theme-giorno/src/components/ImageBlock.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Img from 'gatsby-image';
-import { getFluidGatsbyImage } from 'gatsby-source-sanity';
+import { getGatsbyImageData } from 'gatsby-source-sanity';
 
 import clientConfig from '../utils/clientConfig';
 
 const ImageBlock = ({ node }) => {
 	if (!node || !node.asset || !node.asset._ref) return null;
-	const fluid = getFluidGatsbyImage(node.asset._ref, {}, clientConfig.sanity);
+	const fluid = getGatsbyImageData(node.asset._ref, {}, clientConfig.sanity);
 
 	return <Img fluid={fluid} fadeIn className="object-contain" />;
 };
