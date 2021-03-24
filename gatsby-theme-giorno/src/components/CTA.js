bce45a5dda913ead9c6bd65653ef662f347c28e7
danielkvist <daniel@dkvist.com>
2021-03-24 21:51:42 +0100

    refactor(gatsby): update cta styles

--- gatsby-theme-giorno/src/components/CTA.js
+++ gatsby-theme-giorno/src/components/CTA.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { navigate } from 'gatsby';
 
 const CTA = ({ title, url }) => {
+	const navigateTo = () => navigate(url);
+
 	return (
 		<div className="flex items-center justify-center mt-9">
 			<button
 				type="button"
-				className="bg-white text-black text-md md:text-lg font-semibold uppercase md:tracking-wide px-6 py-3 rounded-md shadow"
-				onClick={() => console.log(url)}
+				className="bg-primary hover:bg-secondary text-invert-text text-md font-semibold uppercase px-6 py-3 tracking-wide rounded-md shadow"
+				onClick={navigateTo}
 			>
 				{title}
 			</button>
