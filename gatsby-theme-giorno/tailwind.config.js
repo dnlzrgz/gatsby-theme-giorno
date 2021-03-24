d2b2e8b241909cd4886258f28473fc1d12a42580
danielkvist <daniel@dkvist.com>
2021-03-24 21:51:21 +0100

    feat(gatsby): add tailwindcss custom palette

--- gatsby-theme-giorno/tailwind.config.js
+++ gatsby-theme-giorno/tailwind.config.js
@@ -1,8 +1,22 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}'],
 	darkMode: false,
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: colors.lightBlue[500],
+				secondary: colors.lightBlue[600],
+				accent: colors.pink[500],
+				background: colors.white,
+				'invert-background': colors.coolGray[900],
+				text: colors.coolGray[900],
+				'invert-text': colors.white,
+				heading: colors.lightBlue[900],
+				neutral: colors.coolGray[700],
+			},
+		},
 	},
 	variants: {
 		extend: {},
