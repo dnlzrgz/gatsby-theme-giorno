c00285308c9df0adaa3aeb5e17bcd20b5e3d6783
danielkvist <daniel@dkvist.com>
2021-04-01 22:32:58 +0200

    refactor(gatsby): improve CTA and submit styles

--- gatsby-theme-giorno/src/components/CTA.js
+++ gatsby-theme-giorno/src/components/CTA.js
@@ -8,7 +8,7 @@ const CTA = ({ title, url }) => {
 		<div className="flex items-center justify-center mt-9">
 			<button
 				type="button"
-				className="bg-primary hover:bg-secondary text-invert-text text-md font-semibold uppercase px-6 py-3 tracking-wide rounded-md shadow"
+				className="bg-primary hover:bg-darker-primary text-invert-text text-md font-semibold uppercase px-6 py-3 tracking-wide outline-none rounded-md shadow"
 				onClick={navigateTo}
 			>
 				{title}
--- gatsby-theme-giorno/src/components/FormSubmit.js
+++ gatsby-theme-giorno/src/components/FormSubmit.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const FormSubmit = ({ onSubmit, children }) => {
 	return (
-		<button
-			type="submit"
-			onClick={onSubmit}
-			className="text-md md:text-lg bg-gray-700 text-white px-6 py-3 rounded-md"
-		>
-			{children}
-		</button>
+		<div className="flex items-center justify-end">
+			<button
+				type="submit"
+				onClick={onSubmit}
+				className="text-md font-semibold uppercase bg-primary hover:bg-darker-primary text-invert-text px-6 py-3 tracking-wide outline-none rounded-md shadow"
+			>
+				{children}
+			</button>
+		</div>
 	);
 };
 
