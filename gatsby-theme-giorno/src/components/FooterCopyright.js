eaf0c16a76966a1d7f879eb1985f647778650c0f
danielkvist <daniel@dkvist.com>
2021-04-14 12:24:43 +0200

    fix(gatsby): fix grid col span on footer copy

--- gatsby-theme-giorno/src/components/FooterCopyright.js
+++ gatsby-theme-giorno/src/components/FooterCopyright.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const FooterCopyright = ({ copy }) => {
 	return (
-		<section className="col-span-1 sm:col-span-2 md:col-span-3">
+		<section className="col-span-3">
 			<main className="text-center px-12">
 				<p>Copyright &copy; - {copy}</p>
 			</main>
