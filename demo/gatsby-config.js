3911657f05d6923e21153d1245d295664701bc61
danielkvist <daniel@dkvist.com>
2021-05-01 19:46:12 +0200

    feat(gatsby): add theme options

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,3 +1,21 @@
+const path = require("path");
+require("dotenv").config({
+  path: path.resolve("./.env"),
+});
+
 module.exports = {
-  plugins: ["gatsby-theme-giorno", "gatsby-plugin-postcss"],
+  siteMetadata: {
+    siteUrl: `https://giorno-demo.netlify.app`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-theme-giorno",
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET_NAME,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+    "gatsby-plugin-postcss",
+  ],
 };
--- gatsby-theme-giorno/gatsby-config.js
+++ gatsby-theme-giorno/gatsby-config.js
@@ -1,12 +1,4 @@
-const path = require("path");
-require("dotenv").config({
-  path: path.resolve("../.env"),
-});
-
-module.exports = {
-  siteMetadata: {
-    siteUrl: `https://giorno-demo.netlify.app`,
-  },
+module.exports = ({ projectId, dataset, token }) => ({
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-postcss",
@@ -14,9 +6,9 @@ module.exports = {
     {
       resolve: "gatsby-source-sanity",
       options: {
-        projectId: process.env.SANITY_PROJECT_ID,
-        dataset: process.env.SANITY_DATASET_NAME,
-        token: process.env.SANITY_TOKEN,
+        projectId,
+        dataset,
+        token,
         watchMode: true,
         overlayDrafts: true,
       },
@@ -24,4 +16,4 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-typescript`,
   ],
-};
+});
