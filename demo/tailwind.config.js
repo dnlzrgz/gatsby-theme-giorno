db32bae64fa363072f5564b9dfff65b1d576ab0b
danielkvist <daniel@dkvist.com>
2021-05-11 20:20:44 +0200

    chore(*): add LICENSE

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -6,8 +6,6 @@ module.exports = {
       colors: {
         primary: "#3b82f6",
         "darker-primary": "#2563eb",
-        accent: "#ec4899",
-        "darker-accent": "#db2777",
         background: "#fff",
         "invert-background": "#1f2937",
         text: "#111827",
