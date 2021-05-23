2bae95c72f72a7fffa62d4bb5a4ba7d18752c30b
danielkvist <daniel@dkvist.com>
2021-05-23 22:26:01 +0200

    feat: improve code block styles

--- gatsby-theme-giorno/src/components/CodeBlock.tsx
+++ gatsby-theme-giorno/src/components/CodeBlock.tsx
@@ -11,9 +11,19 @@ interface Props {
 
 const CodeBlock: FC<Props> = ({ node }) => {
   return (
-    <SyntaxHighlighter language={node.language} style={style}>
-      {node.code}
-    </SyntaxHighlighter>
+    <div className="text-lg my-4">
+      <div className="flex justify-between items-center py-5 px-5 bg-gray-900 rounded-t-xl">
+        <div className="flex items-center space-x-2">
+          <div className="w-4 h-4 border-2 rounded-full border-red-500 bg-red-500" />
+          <div className="w-4 h-4 border-2 rounded-full border-white-500 bg-white" />
+          <div className="w-4 h-4 border-2 rounded-full border-green-500 bg-green-500" />
+        </div>
+        <p className="self-start text-gray-50">{node.language}</p>
+      </div>
+      <SyntaxHighlighter showLineNumbers={true} wrapLines={true} language={node.language} style={style}>
+        {node.code}
+      </SyntaxHighlighter>
+    </div>
   );
 };
 
