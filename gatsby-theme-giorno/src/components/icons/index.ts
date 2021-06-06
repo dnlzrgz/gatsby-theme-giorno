eb82ff75725a862ddef9d6ed07a3e1355225b017
danielkvist <daniel@dkvist.com>
2021-06-06 13:12:41 +0200

    refactor: remove unused icon

--- gatsby-theme-giorno/src/components/icons/index.ts
+++ gatsby-theme-giorno/src/components/icons/index.ts
@@ -1,7 +1,6 @@
-import AtIcon from "./AtIcon";
 import CloseMenuIcon from "./CloseMenuIcon";
 import LocationIcon from "./LocationIcon";
 import MenuIcon from "./MenuIcon";
 import PhoneIcon from "./PhoneIcon";
 
-export { AtIcon, CloseMenuIcon, LocationIcon, MenuIcon, PhoneIcon };
+export { CloseMenuIcon, LocationIcon, MenuIcon, PhoneIcon };
